52c7b8522bb6e685430d9acd5f4983715ae114f5
MaximeG9 <gurbala.maxime@gmail.com>
2023-11-02 14:49:10 +0100

    fin intro react

--- src/Time.js
+++ src/Time.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import zaWarudo from "./zaWarudo.gif";
 
 function Time() {
   const [time, setTime] = useState(0);
@@ -8,12 +9,35 @@ function Time() {
       setTime(time + 1);
     }, 1000);
 
+    if(time === 10){
+      clearInterval(interval);
+      document.body.classList.add("backgroundBlack");
+
+      document.querySelectorAll("p").forEach((e) => (e.style.color = "white"));
+
+      let h1 = document.querySelector("h1");
+      h1.style.color = "white";
+
+      let h2 = document.createElement("h2");
+      h2.innerHTML = "ZA WARUDO !!!";
+      h2.style.color = "white";
+      h2.classList.add("bigSize");
+
+      document.querySelector(".time").appendChild(h2);
+
+      let imgElement = document.createElement("img");
+      imgElement.setAttribute("src", zaWarudo);
+      imgElement.setAttribute("alt", "za Warudo");
+
+      document.querySelector(".time").appendChild(imgElement);
+    }
+
     return () => clearInterval(interval);
   })
   
   return (
     <>
-      <section>
+      <section className="time">
         <p>Nombre de secondes passées : {time}</p>
       </section>
     </>
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,16 +1,16 @@
-import Click from "./Click";
+import Click from "./Click"
 
 function Welcome(props) {
   return (
     <>
-      <section>
-        <h1>
-          Welcome {props.firstname} {props.lastname} !
-        </h1>
-        <Click />
-      </section>
+      <section className="welcome">
+            <h1>
+                Welcome {props.firstname} {props.lastname} !
+            </h1>
+            <Click />
+    </section>
     </>
-  );
+  )
 }
 
-export default Welcome;
+export default Welcome
